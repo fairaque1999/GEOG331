2ee10bc1cbf165556086143860265cb20307ed1b
fairaque1999 <ishraque@colgate.edu>
2022-02-01 18:33:20 -0500

    code for HW 1

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,17 @@
+# Response to question 1
 print("Hello World")
+
+# Response to Question 2
+
+vec.2 <- c(2000, 13, 45, 67, 97)
+vec.1 <- c(1, 2, 3, 4, 7)
+
+# Adding the two vectors
+vec.1 + vec.2
+
+# Multiplying the vectors
+vec.1 * vec.2
+
+# Taking a sum of the elements of each vector and adding them up
+add <- sum(vec.1)+sum(vec.2)
+add
