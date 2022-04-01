5c486107d0ae94d2561d62c2ceea37e932a32aa5
fairaque1999 <53129458+fairaque1999@users.noreply.github.com>
2022-04-01 11:41:42 -0400

    HW4 done

--- Activity4.R
+++ Activity4.R
@@ -18,7 +18,20 @@ library(ggplot2)
 
 # hint: consider using a list, and also new vectors for regression variables
 
+# filtering out the iris dataset to include only versicolor
+iris.versi <- iris[iris$Species == "versicolor",]
 
+# making dataframes of the columns to be used in regression
+vars_dep<- data.frame(iris.versi$Sepal.Length, iris.versi$Petal.Length, iris.versi$Sepal.Length)
+colnames(vars_dep) <- c("Sepal.Length", "Petal.Length", "Sepal.Length")
+vars_indep <- data.frame(iris.versi$Sepal.Width, iris.versi$Petal.Width, iris.versi$Petal.Length)
+colnames(vars_indep) <- c("Sepal.Width", "Petal.Width", "Petal.Length")
+# creating a list to store the regression tables
+reg_table_list <- list(0)
+
+for (i in 1:3){
+  reg_table_list[[i]] <- lm(vars_dep[,i]~vars_indep[,i])
+}
 
 #####################################
 ##### Part 2: data in dplyr     #####
@@ -28,11 +41,7 @@ library(ggplot2)
 #to a new iris data frame
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
-iris_new <- iris
-
-# Inner join?
-
-
+iris_new <- full_join(iris, height)
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
@@ -42,16 +51,32 @@ iris_new <- iris
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
-
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
+  geom_point()
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
-
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width)) + 
+  geom_point() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+ggplot(iris, aes(x=Sepal.Length, y=Sepal.Width, color = Species, size = Petal.Length)) + 
+  geom_point() +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  labs(x = "Sepal Length (cm)",
+       y = "Sepal Width (cm)",
+       title = "Variation of Sepal Size")
 
 #####################################
 ##### Question: how did         #####
 ##### arguments differ between  #####
 ##### plot and ggplot?          #####
 #####################################		
+
+# For the base r plot, it only plotted with the standard plot(x,y) scheme. In ggplot,
+# it is necessary to create a new dataframe to start the ggplot and columns of the dataframe
+# are used as arguments of aes() inside the ggplot function to set the data to plot.
+# furthermore, the elements in a ggplot plotn are set in layers, where ggplot() makes up the
+# base layer and we can keep adding elements with functions like geom_point(), geom_line(), etc.
+# The elements of a ggplot plot are combined together using a + sign.
